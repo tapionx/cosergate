7749f58869bb9b584df1b1c2745c0b367e8a591a
mariuccio <mariotaddei@rocketmail.com>
2014-03-17 18:06:53 +0100

    index

--- cosergate/urls.py
+++ cosergate/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'cosergate.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+	url(r'^', include('web.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
