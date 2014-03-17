e18ad059cef101123504496242a0fa8abff7b5cf
mariuccio <mariotaddei@rocketmail.com>
2014-03-17 18:34:13 +0100

    background and icon

--- cosergate/urls.py
+++ cosergate/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,4 +10,4 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 	url(r'^', include('web.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
